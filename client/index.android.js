24b84163735712fcdb019bd7c9c1a2d31b090e6d
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2017-06-20 22:18:27 +0300

    moved initial sessionCheck to router
    updated redux

--- client/index.android.js
+++ client/index.android.js
@@ -9,24 +9,11 @@ import {AppRegistry} from "react-native";
 import {Router} from "./components/router";
 import Provider from "react-redux/es/components/Provider";
 import {MessengerStore} from "./utils/redux-stores";
-import {chatSocket, createMessage} from "./communication/websocket-client";
-import {SessionAsyncStorage} from "./utils/async-storage";
-import {ReduxRouter} from "./utils/router";
+
 
 export default class E2essage extends Component {
 	constructor(props) {
 		super(props);
-	}
-
-	componentWillMount() {
-		SessionAsyncStorage.getSession().then(function (session) {
-			session = JSON.parse(session);
-			if (!session || !Object.keys(session).length) {
-				ReduxRouter.go('login');
-				return;
-			}
-			chatSocket.sendMessage(createMessage('authenticate', session))
-		});
 	}
 
 	render() {
